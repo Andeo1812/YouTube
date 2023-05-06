c3ec950ee9c51c5822058f4eada13cab24792da6
Andeo1812 <alexanforall@mail.ru>
2023-05-06 22:55:46 +0300

    remove other hw

--- staticwebserver/docker/nginx.Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM nginx
-
-WORKDIR /etc/nginx
-
-COPY /configs/nginx.conf /etc/nginx/nginx.conf
-COPY  /tests /etc/nginx/static/tests
--- staticwebserver/project/include/http_handler.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <string>
-
-class HTTPHandler {
- public:
-  std::string handle(const std::string &request) const;
-};
